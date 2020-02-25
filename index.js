2f01aed21ce34883412b251de655d643ddf0c529
kush11 <kush.codingmart.com>
2020-02-25 12:53:15 +0530

    added navigation

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/config/routes/Navigation';
+import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
